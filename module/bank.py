07962ee7c411d28ed174c60ea96e1c4a49a26127
Kris <kristian_dilov@abv.bg>
2023-03-04 14:29:05 +0000

    use black

--- module/bank.py
+++ module/bank.py
@@ -1,15 +1,14 @@
-#Kata used: https://kata-log.rocks/banking-kata
+# Kata used: https://kata-log.rocks/banking-kata
 from typing import Optional
+
 
 class BankAccount:
     # Green
     # ammount = 0
 
-    #Refactor
+    # Refactor
     def __init__(self, ammount: Optional[int] = 0):
-        self.ammount = ammount 
-
+        self.ammount = ammount
 
     def deposit(self, amm):
         self.ammount += amm
-
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,35 +2,43 @@
 from hamcrest import assert_that, equal_to
 from module.bank import BankAccount
 
-#Red
+# Red
 """Test that 
 When I create an account
 And I dont deposit it with any money
 Then I will have a bank account that is empty"""
 
+
 def test_create_bank_account():
-    
+
     bank_acc = BankAccount()
 
     assert_that(bank_acc.ammount, equal_to(0))
 
+
 """Test that 
 When I deposit some money into the account
 Then the new ammount will be as expected"""
+
+
 def test_deposit_to_bank_account():
     empty_b_a = BankAccount()
     empty_b_a.deposit(100)
     assert_that(empty_b_a.ammount, equal_to(100))
 
+
 """Test that
 When I deposit muliple times to the same account 
 Then I will get the expected value"""
+
+
 def test_multiple_deposits():
     empty_b_a = BankAccount()
     empty_b_a.deposit(100)
     empty_b_a.deposit(200)
 
     assert_that(empty_b_a.ammount, equal_to(300))
+
 
 """Test that
 When I deposit into seperate BA
