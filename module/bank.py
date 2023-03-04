5c693d1753a01a1736092ab544469dcd99748bc9
Kris <kristian_dilov@abv.bg>
2023-03-04 13:56:46 +0000

    tests+refactor

--- module/bank.py
+++ module/bank.py
@@ -1,3 +1,15 @@
+#Kata used: https://kata-log.rocks/banking-kata
+from typing import Optional
 
 class BankAccount:
-    pass
+    # Green
+    # ammount = 0
+
+    #Refactor
+    def __init__(self, ammount: Optional[int] = 0):
+        self.ammount = ammount 
+
+
+    def deposit(self, amm):
+        self.ammount += amm
+
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,7 +2,7 @@
 from module.bank import BankAccount
 from hamcrest import assert_that, equal_to
 
-
+#Red
 """Test that 
 When I create an account
 And I dont deposit it with any money
@@ -13,3 +13,30 @@ def test_create_bank_account():
     bank_acc = BankAccount()
 
     assert_that(bank_acc.ammount, equal_to(0))
+
+"""Test that 
+When I deposit some money into the account
+Then the new ammount will be as expected"""
+def test_deposit_to_bank_account():
+    empty_b_a = BankAccount()
+    empty_b_a.deposit(100)
+    assert_that(empty_b_a.ammount, equal_to(100))
+
+"""Test that
+When I deposit muliple times to the same account 
+Then I will get the expected value"""
+def test_multiple_deposits():
+    empty_b_a = BankAccount()
+    empty_b_a.deposit(100)
+    empty_b_a.deposit(200)
+
+    assert_that(empty_b_a.ammount, equal_to(300))
+
+"""Test that
+When I deposit into seperate BA
+Then I will get the expected ammounts"""
+
+
+"""Test that
+When I deposit money into the account
+There is a timestamp attached to the transaction"""
